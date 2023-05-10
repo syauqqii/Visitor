8cc26d74b2ea06f406379343dcf9da8ead80ac42
Qii <57295427+syauqqii@users.noreply.github.com>
2023-05-10 19:50:38 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+from argparse import ArgumentParser
+from os import system
+from requests import get
+from threading import Thread
+from time import time
+
+FlagOK     = 0
+FlagReject = 0
+
+def GetURL(url):
+	global FlagOK, FlagReject
+	response = get(url)
+	if response.status_code == 200:
+		FlagOK += 1
+	else:
+		FlagReject += 1
+
+if __name__ == "__main__":
+	try:
+		parser = ArgumentParser(description='Increase Visitor GitHub Badge')
+
+		parser.add_argument('-U', '--username', dest='username', type=str, help='Username GitHub kalian.')
+		parser.add_argument('-I', '--iterasi', dest='iterasi', type=int, help='Iterasi untuk perulangan berapa x program diulang.')
+
+		args = parser.parse_args()
+
+		if args.username is None:
+			args.username = 'syauqqii'
+		if args.iterasi is None:
+			args.iterasi = 10
+
+		LinkBadge = f"https://visitor-badge.laobi.icu/badge?page_id={args.username}"
+
+		TempThread = []
+		WaktuMulai = time()
+
+		for i in range(args.iterasi):
+			for j in range(7):
+				thread = Thread(target=GetURL, args=[LinkBadge])
+				TempThread.append(thread)
+				thread.start()
+
+			for thread in TempThread:
+				thread.join()
+
+			system("cls||clear")
+
+			print(f"\n > Iterasi: [{i+1}/{args.iterasi}] | Username: {args.username}\n")
+			print(f"   Berhasil Terkirim : {FlagOK}")
+			print(f"   Gagal Terkirim    : {FlagReject}")
+			TempThread.clear()
+		print(f"\n > Berhasil menyelesaikan {args.iterasi*7} request dalam waktu {time()-WaktuMulai:.2f} detik.")
+	except KeyboardInterrupt:
+		print(f"\n > Program berjalan selama {time()-WaktuMulai:.2f} detik.")
+		exit(0)
